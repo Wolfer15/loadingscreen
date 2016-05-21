8309b3f5b17abf3fa91ac2cd1c372f48ebf718d7
Wolfer15 <wolfergamer1234@hotmail.com>
2016-05-21 02:43:34 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+
+//General
+$APIKey = "0232551360F9558AC2E58C9CC8DD3465"; // Go get your API here: http://steamcommunity.com/dev/apikey
+
+$load_name ="The Tagliano Network"; // For your Browser Name
+$community_name ="The Tagliano Network"; // Your community/server name. The one in the middle
+$community_url = "http://steamcommunity.com/groups/TheTaglianoNetwork"; // Url for your users to know where to go
+$intro = TRUE; // If you want to turn this off, type FALSE instead
+// Introduction
+$tagline = " A long time ago, in a server far,<br> far away...."; // The intro text in the beginning of the animation
+// Above Server Name
+$sub_title_1 = "Welcome to The Tagliano Network"; // Above your server name
+// Under Server Name
+$sub_title_2 = " read the rules"; // Below your server name
+
+//Bottom Part
+$left_bottom_title = "About Us";
+$left_bottom_sub_title = "$community_url for more info";
+$right_bottom_title = "Our Server";
+$right_bottom_sub_title = "General information about the server";
+
+$left_bottom_text = "Welcome to The Tagliano Network Star Wars Roleplay Server, We are dedicated to Serious Roleplay and we aim to stay within the Boundaries of Star Wars Lore, Great Server for any loving fans of the expanding universe of Star Wars, we are expanding with lore weekly with new roles and missions, Have fun and enjoy our server!  We have Great Friendly Admins/Moderators who are Regularly on and are there to help with any inconviniences caused during your Experience on our server.";
+
+?>
